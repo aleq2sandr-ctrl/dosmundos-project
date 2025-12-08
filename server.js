81f2b29d30ef3e5fc78b65b4d7a2592d0fdeff06
aleq2sandr-ctrl <aleq2sandr@gmail.com>
2025-12-08 13:31:31 +0700

    1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import saveTranscript from './api/save-transcript.js';
+import handleTelegramPreview from './api/telegram-preview.js';
 
 dotenv.config();
 
@@ -21,6 +22,9 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.text({ limit: '50mb' }));
+
+// Telegram Instant View Preview Route
+app.get('/preview/:lang/:episodeSlug', handleTelegramPreview);
 
 // API routes
 app.post('/api/save-transcript', async (req, res) => {
